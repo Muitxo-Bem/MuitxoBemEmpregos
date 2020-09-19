85886f06b157635c38c2694b4392cdd19b58f402
Davidbr1to <david.nrpg@gmail.com>
2020-09-19 19:34:01 -0300

    Adicionando Model e Migration de Idioma

---
+++ database/migrations/2020_09_19_200845_create_idiomas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdiomasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idiomas', function (Blueprint $table) {
+            $table->unsignedBigInteger('curriculo_id');
+            $table->string('Nome');
+
+            $table->timestamps();
+
+            $table->foreign('curriculo_id')->references('id')->on('curriculos');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idiomas');
+    }
+}
