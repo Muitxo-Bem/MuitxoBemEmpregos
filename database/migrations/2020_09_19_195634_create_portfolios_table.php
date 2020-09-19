09e90af612aa4a1abd99d17219468167e8ba5a0b
Davidbr1to <david.nrpg@gmail.com>
2020-09-19 19:35:43 -0300

    Adicionando Model e Migration de Portfólio

---
+++ database/migrations/2020_09_19_195634_create_portfolios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->unsignedBigInteger('candidato_id');
+            $table->string('link-portfolio')->unique();
+            //$table->blob('pdf-porfolio');
+
+            $table->timestamps();
+
+            $table->foreign('candidato_id')->references('id')->on('candidatos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
