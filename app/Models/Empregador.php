9ff45a5077560ddac1aa44bb74eb59d156f6a25b
BASDiniz <brunodiniz1818@gmail.com>
2020-09-30 19:57:22 -0300

    Adicionando validaçao Empregador

--- app/Models/Empregador.php
+++ app/Models/Empregador.php
@@ -13,6 +13,20 @@ class Empregador extends Model
 
     protected $hidden = ['senha'];
 
+    public static $rules =  [
+                            'nome' => 'required|min:5|max:100',
+                            'cpf' => 'required|digits:11',
+                            'email' => 'required|email',
+                            'senha' => 'required|min:8', //Confirmação de password? Usar confirmed
+                            ];
+
+    public static $messages = [
+                              'nome.*' => 'O campo Nome é obrigatório e deve ter entre 5 e 100 caracteres',
+                              'cpf.*' => 'O campo CPF é obrigatório e deve conter 11 digitos',
+                              'email.*' => 'O campo Email é obrigatório e deve ser válido',
+                              'senha.*' => 'O campo Senha é obrigatório e deve conter no mínimo 8 caracteres',
+                              ];
+
     public function telefones(){
         return $this->hasMany('App\Models\Telefone');
     }
