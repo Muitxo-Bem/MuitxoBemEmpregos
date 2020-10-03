58c5fc3edbf0dc3400e6c73084667245a7040298
Isaac Braga <braga.isaacp@gmail.com>
2020-10-02 22:24:32 -0300

    Adicionando validator de Idioma

---
+++ app/Validator/IdiomaValidator.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Validator;
+
+
+class IdiomaValidator
+{
+    public static function validate($data){
+        $validator = \Validator::make($data, \App\Models\Idioma::$rules,\App\Models\Idioma::$messages);
+        $curr = Curriculo::find($data['curriculo_id']);
+        if(is_null($curr)){
+            $validator->errors()->add('curriculo_invalido','Currículo Inválido');
+        }
+        if(!$validator->errors()->isEmpty())
+            throw new ValidationException($validator, "Erro na validação do idioma");
+        return $validator;
+    }
+}
