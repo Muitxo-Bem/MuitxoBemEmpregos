a7fa40f9e3b112cc99d481e76bba8ed11d2f327e
Isaac Braga <braga.isaacp@gmail.com>
2020-10-02 22:52:04 -0300

    Adicionando Validator de SubArea

--- app/Models/SubArea.php
+++ app/Models/SubArea.php
@@ -11,6 +11,14 @@ class SubArea extends Model
 
     protected $fillable = ['area_formacao_id','nome'];
 
+    public static $rules = ['area_formacao_id' => 'required',
+        'nome' => 'required|min:5|max:30',
+    ];
+
+    public static $messages = ['area_formacao_id.*'=>'Id da área de formação Vazia',
+        'nome' => 'O campo nome é obrigatório e deve ter entre 5 e 30 caracteres'
+    ];
+
     public function dono(){
         return $this->belongsTo('App\Models\AreaFormacao','area_formacao_id');
     }
