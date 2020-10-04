7b708bfc85214134ee8f98536f2643dcb3d1dcc0
BASDiniz <brunodiniz1818@gmail.com>
2020-10-04 13:31:49 -0300

    Adicionando Testes de Unit de CandidatoValidator

--- app/Validator/CandidatoValidator.php
+++ app/Validator/CandidatoValidator.php
@@ -8,16 +8,11 @@ class CandidatoValidator{
 
     public static function validate($data){
         $validatorCandidato = \Validator::make($data, \App\Models\Candidato::$rules, \App\Models\Candidato::$messages);
-        $validatorTelefone = \Validator::make($data, \App\Models\Telefone::$rules, \App\Models\Telefone::$messages);
-        $validatorEndereco = \Validator::make($data, \App\Models\Endereco::$rules, \App\Models\Endereco::$messages);
-
+        //dd($validatorCandidato->errors());
+        //dd($data);
         if(!$validatorCandidato->errors()->isEmpty())
             throw new ValidationException($validatorCandidato, "Erro na validação do Candidato");
-        if(!$validatorTelefone->errors()->isEmpty())
-            throw new ValidationException($validatorTelefone, "Erro na validação do Telefone");
-        if(!$validatorEndereco->errors()->isEmpty())
-            throw new ValidationException($validatorEndereco, "Erro na validação do Endereço");
 
-        return [$validatorCandidato, $validatorTelefone, $validatorEndereco];
+        return $validatorCandidato;
     }
 }
---
+++ tests/Unit/CandidatoValidatorTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Candidato;
+use App\Validator\CandidatoValidator;
+use App\Validator\ValidationException;
+use Tests\TestCase;
+
+class CandidatoValidatorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCandidatoSemNome(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['nome' => '']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testNomeCandidatoMenorQueTres(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['nome' => 'ab']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testNomeCandidatoMaiorQueCem(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['nome' => 'ensP65JekrmDCq9CWCK2RnqCc8D5dqpCq9CWCK2RnqCc8D5dqpCq9CWCK2RnqCc8D5dqpCq9CWCK2RnqCc8D5dqpCq9CWCK2RnqCc8D5dqpClJDzYjl30EIhOB6HUE8R2syeVN20']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testCandidatoSemCpf(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['cpf' => '']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testCpfCandidatoDiferenteDeQuatorze(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['cpf' => '321']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testCandidatoSemEmail(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['email' => '']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testEmailCandidatoInvalido(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['email' => 'uniuEmail']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testCandidatoSemSenha(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['senha' => '']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testSenhaCandidatoMenorQueOito(){
+        $this->expectException(ValidationException::class);
+        $candidato = Candidato::factory()->make(['senha' => '123321']);
+        CandidatoValidator::validate($candidato->toArray());
+    }
+
+    public function testCandidatoCorreto(){
+        $candidato = Candidato::factory()->make();
+        $candidato->senha_confirmation = $candidato->senha;
+        CandidatoValidator::validate($candidato->toArray());    
+        $this->assertTrue(True);
+    }
+}
