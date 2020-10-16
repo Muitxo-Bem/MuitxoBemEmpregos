b0b3b44e43eaafd580b15e99392b85a184c7765c
Davidbr1to <david.nrpg@gmail.com>
2020-10-16 01:44:04 -0300

    Atualizando Validator de Vaga e Portfolio

--- app/Validator/VagaEmpregoValidator.php
+++ app/Validator/VagaEmpregoValidator.php
@@ -8,10 +8,6 @@ use App\Models\VagaEmprego;
 class VagaEmpregoValidator{
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\VagaEmprego::$rules,\App\Models\VagaEmprego::$messages);
-        $emp = Empregador::find($data['empregador_id']);
-        if(is_null($emp)){
-            $validator->errors()->add('empregador_invalido','Empregador Inválido');
-        }
         if($data['quantidade_de_vagas'] < 0){
             $validator->errors()->add('quantidade_invalida','Quantidade de vagas negativa');
         }
