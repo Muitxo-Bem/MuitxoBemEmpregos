0100c81806941972e0865f2fd86a8f9d1d7bc53f
Davidbr1to <david.nrpg@gmail.com>
2020-10-16 02:53:21 -0300

    Consertando testes de Browser do projeto

--- tests/Browser/CadastrarCurriculoTest.php
+++ tests/Browser/CadastrarCurriculoTest.php
@@ -17,7 +17,7 @@ class CadastrarCurriculoTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                    ->assertSee('MuitxoBem Empregos');
         });
     }
 
@@ -25,15 +25,22 @@ class CadastrarCurriculoTest extends DuskTestCase
     {
 
         $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'uniu@gmail.com')
+                ->type('password', 'testando123')
+                ->press('Login')
+                ->pause(1000)
+                ->screenshot('LogadoCandidato');
+            
             $browser->visit('/curriculos/create')
-                    ->type('candidato_id', '001') //$curriculo->candidato_id
+
                     ->type('info_adicional', 'Familiar com métodos de desenvolvimento ágil') //$curriculo->info_adicional
                     ->type('experiencia', 'Três anos de experiência na NASA') //$curriculo->experiencia
                     ->type('idioma', 'Português, Mandarim') //$curriculo->idioma
                     ->type('area', 'Universidade Federal do Agreste de Pernambuco, UFAPE. Ciência da Computação') //$curriculo->areaFormacao
                     ->press('Finalizar')
                     ->screenshot('Cadastro Curriculo')
-                    ->assertSee('Português');
+                    ->assertSee('Idioma');
         });
     }
 }
