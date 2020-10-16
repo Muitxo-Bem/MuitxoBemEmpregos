2b7ed4b648a8746532ecb04480208027edb0c956
Davidbr1to <david.nrpg@gmail.com>
2020-10-16 01:44:51 -0300

    Atualizando models de Curriculo, Porfolio e Vaga

--- app/Models/VagaEmprego.php
+++ app/Models/VagaEmprego.php
@@ -10,19 +10,17 @@ class VagaEmprego extends Model
     use HasFactory;
 
     protected $table = 'vaga_empregos';
-    protected $fillable = ['ativa','faixa_salarial','requisitos','descricao','empregador_id',
+    protected $fillable = ['ativa','faixa_salarial','requisitos','descricao',
                            'local_de_trabalho','nome','diferenciais','quantidade_de_vagas'];
     public static $rules = ['faixa_salarial' => 'regex:/^\d+(\.\d{1,2})?$/',
                             'requisitos' => 'required',
                             'descricao' => 'required',
-                            'empregador_id'=> 'required',
                             'local_de_trabalho' => 'required',
                             'nome' => 'required',
                             'quantidade_de_vagas' => 'required|integer'];
     public static $messages = ['faixa_salarial.*' => "Faixa Salarial Inválida",
                                'requisitos.*' => 'Requisitos são necessários',
                                'descricao.*'=> 'A descrição é necessária',
-                               'empregador_id.*' => 'Id do Empregador Vazia',
                                'local_de_trabalho.*' => 'Local de Trabalho é necessário',
                                'nome.*' => "Nome da Vaga é necessário",
                                'quantidade_de_vagas.required'=>"A quantidade de vagas é necessária",
